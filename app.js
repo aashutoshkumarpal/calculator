4935942511eb371e71187d3ca53dc4a06eadd469
Aashutosh kumar pal <aashutoshkumarpal022@gmail.com>
2024-07-19 12:33:24 +0530

    calculator

---
+++ app.js
@@ -0,0 +1,65 @@
+let display = document.getElementById('display');
+let buttons = document.querySelectorAll('button[type="button"]');
+
+let calculator = {
+    displayValue: '',
+    firstOperand: '',
+    operator: '',
+    secondOperand: '',
+    result: '',
+
+    init: function() {
+        buttons.forEach(button => {
+            button.addEventListener('click', this.handleClick);
+        });
+    },
+
+    handleClick: function(event) {
+        let value = event.target.value;
+
+        if (value === 'C') {
+            calculator.clear();
+        } else if (value === '=') {
+            calculator.calculate();
+        } else if (value === '+' || value === '-' || value === '*' || value === '/') {
+            calculator.setOperator(value);
+        } else {
+            calculator.appendValue(value);
+        }
+
+        display.value = calculator.displayValue;
+    },
+
+    clear: function() {
+        this.displayValue = '';
+        this.firstOperand = '';
+        this.operator = '';
+        this.secondOperand = '';
+        this.result = '';
+    },
+
+    setOperator: function(operator) {
+        this.operator = operator;
+        this.firstOperand = this.displayValue;
+        this.displayValue = '';
+    },
+
+    appendValue: function(value) {
+        if (this.displayValue === '0') {
+            this.displayValue = value;
+        } else {
+            this.displayValue += value;
+        }
+    },
+
+    calculate: function() {
+        this.secondOperand = this.displayValue;
+        let result = eval(`${this.firstOperand} ${this.operator} ${this.secondOperand}`);
+        this.displayValue = result;
+        this.firstOperand = '';
+        this.operator = '';
+        this.secondOperand = '';
+    }
+};
+
+calculator.init();
